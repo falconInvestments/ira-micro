eacf09b47d720c978f7622b697ad66bb6682a9c2
James Morical <77235874+jkmor31@users.noreply.github.com>
2022-01-18 13:46:57 -0600

    update port/docker info

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: 'db-inst-1.cdg9ngplsawh.us-west-2.rds.amazonaws.com', // your endpoint
     USER: 'postgres_jkmor31', // your username
-    PASSWORD: 'falconinvestments', // your password
+    PASSWORD: 'changepassword', // your password
     DB: 'ira_postgres_db', // your db name
     dialect: 'postgres',
     }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = 5280
 app.use(express.json())
 
 const routers = require('./routes/userRouter.js')
