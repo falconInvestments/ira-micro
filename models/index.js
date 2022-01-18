c459295852f97d5b1932a58a054a990eb781c429
James Morical <77235874+jkmor31@users.noreply.github.com>
2022-01-18 15:46:24 -0600

    change db to heroku

--- models/index.js
+++ models/index.js
@@ -2,14 +2,29 @@ const dbConfig = require('../config/dbConfig')
 
 const { Sequelize, DataTypes } = require('sequelize')
 
-const sequelize = new Sequelize(
-    { 
-        database: dbConfig.DB,
-        username: dbConfig.USER,
-        password: dbConfig.PASSWORD,
-        dialect: dbConfig.dialect,
-        host: dbConfig.HOST,
-    })
+// construct the sequelize object using the constructor
+let sequelize = null;
+
+    if (process && process.env.DATABASE_URL) {
+        sequelize = new Sequelize(process.env.DATABASE_URL, {
+            dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+                }
+              }
+            }
+        );
+    } else {
+       sequelize = new Sequelize(
+        { // use imported configurations from dbConfig
+            database: process.env.DATABASE,
+            username: process.env.USER,
+            password: process.env.PASSWORD,
+            dialect: dbConfig.dialect,
+            host: process.env.HOST,
+        })
+    }
 
 
 sequelize.authenticate()
