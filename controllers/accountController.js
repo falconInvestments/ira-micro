4ba5c836d7b550766f1b6040048f7e2bb2db7b06
James Morical <77235874+jkmor31@users.noreply.github.com>
2022-01-26 11:27:06 -0600

    add catch

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,51 +1,56 @@
-const db = require('../models/index')
+const db = require("../models/index");
 
 const Account = db.Accounts;
 
 const addAccount = async (req, res) => {
+  try {
     let input_data = {
-        label: req.body.label,
-        balance: req.body.balance,
-        annualContribution: req.body.annualContribution,
-        userId: req.body.userId,
-     }
-     const account = await Account.create(input_data)
-     
-     res.status(200).send(account)
-}
+      label: req.body.label,
+      balance: req.body.balance,
+      annualContribution: req.body.annualContribution,
+      userId: req.body.userId,
+    };
+    const account = await Account.create(input_data);
+
+    res.status(200).send(account);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const getAllAccounts = async (req, res) => {
-
-    let accounts = await Account.findAll({include: db.Investments})
-    res.status(200).send(accounts)
-}
+  let accounts = await Account.findAll({ include: db.Investments });
+  res.status(200).send(accounts);
+};
 
 const getOneAccount = async (req, res) => {
-    
-    let id = req.params.id
+  let id = req.params.id;
 
-    let accounts = await Account.findOne({where: {id: id}, include: db.Investments})
-    res.status(200).send(accounts)
-}
+  let accounts = await Account.findOne({
+    where: { id: id },
+    include: db.Investments,
+  });
+  res.status(200).send(accounts);
+};
 
 const updateAccount = async (req, res) => {
-    let id = req.params.id
+  let id = req.params.id;
 
-    const account = await Account.update(req.body, { where: {id: id}})
-    res.status(200).send(account)
-}
+  const account = await Account.update(req.body, { where: { id: id } });
+  res.status(200).send(account);
+};
 
 const deleteAccount = async (req, res) => {
-    let id = req.params.id
+  let id = req.params.id;
 
-    await Account.destroy({where :{id: id}})
-    res.status(200).send(`account with id: ${id} is deleted`)
-}
+  await Account.destroy({ where: { id: id } });
+  res.status(200).send(`account with id: ${id} is deleted`);
+};
 
 module.exports = {
-    addAccount,
-    getAllAccounts,
-    getOneAccount,
-    updateAccount,
-    deleteAccount
-}
+  addAccount,
+  getAllAccounts,
+  getOneAccount,
+  updateAccount,
+  deleteAccount,
+};
