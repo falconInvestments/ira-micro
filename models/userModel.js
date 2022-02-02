0c6514b4d4b0851e643a00e8430a41907671f723
James Morical <77235874+jkmor31@users.noreply.github.com>
2022-02-02 11:27:15 -0600

    autoincrement true

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('user', {
         id: {
             type: DataTypes.INTEGER,
-            autoIncrement: false,
+            autoIncrement: true,
             allowNull: false,
             primaryKey: true
         },
